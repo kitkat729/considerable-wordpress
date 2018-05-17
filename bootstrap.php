9b5679a0d65318527be2704d08c4b8bb525389ca
Hoi Yuen <hoi@insidehook.com>
2018-05-17 23:44:57 +0000

    feature-#2 added plugin bootstrap and god class

---
+++ bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package Recent_Articles
+ * @version 0.1.0
+ * @since
+ * @author
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/*
+Plugin Name: Recent Articles
+Description: Display recent articles at the bottom of every article
+Version: 0.1.0
+Author: Hoi Kit Yuen
+Author URI: https://www.linkedin.com/in/hoi-yuen-08980775/
+*/
+
+defined( 'ABSPATH' ) or die( 'Direct access not allowed');
+
+
+define( 'RA__MINIMUM_WP_VERSION', '4.4' );
+define( 'RA__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+require_once( 'class-recent-articles.php' );
+
+register_activation_hook( __FILE__, array( 'Recent_Articles', 'onActivation' ) );
+register_deactivation_hook( __FILE__, array( 'Recent_Articles', 'onDeactivation') );
+register_uninstall_hook( __FILE__, array( 'Recent_Articles', 'onUninstall') );
+
---
+++ class-recent-articles.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package Recent_Articles
+ */
+
+
+// main plugin file
+final class Recent_Articles {
+
+
+	public static function onActivation() {
+
+	}
+
+	public static function onDeactivation() {
+
+	}
+
+	public static function onUninstall() {
+		// remove plugin options
+	}
+
+}
